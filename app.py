b8068ff20f2b08b4c5344c7937f5926741418c47
Ryan Cornel <ry.cornel@students.makeschool.com>
2021-04-13 16:41:08 -0700

    Commit 9
    Cleaned up choose_froyo() & show_froyo_ results() functions.

--- app.py
+++ app.py
@@ -18,22 +18,15 @@ def homepage():
 @app.route('/froyo')
 def choose_froyo():
     """Shows a form to collect the user's Fro-Yo order."""
-    return """
-    <form action="/froyo_results" method="GET">
-        What is your favorite Fro-Yo flavor? <br/>
-        <input type="text" name="flavor"><br/>
-        Which toppings would you like? <br/>
-        <input type="text" name="toppings"><br/>
-        <input type="submit" value="Submit!">
-    </form>
-    """
-
+    return render_template('froyo_form.html')
 
 @app.route('/froyo_results')
 def show_froyo_results():
-    users_froyo_flavor = request.args.get('flavor')
-    users_froyo_toppings = request.args.get('toppings')
-    return f'You ordered {users_froyo_flavor} flavored Fro-Yo, with {users_froyo_toppings} toppings!'
+    
+    return render_template('froyo_results.html',
+        users_froyo_flavor = request.args.get('flavor'),
+        users_froyo_toppings = request.args.get('toppings')
+    )
 
 
 @app.route('/favorites')
